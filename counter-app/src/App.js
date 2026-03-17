7e3723f3c4eb5b660b83b85c052d4669f07ed226
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-03-17 19:14:19 +0530

    final

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,9 +1,39 @@
 
 import './App.css';
-
+import { useState } from "react";
 function App() {
+  
+  const [count, setCount]=useState(0);
+
+  function increaseHandler() {
+    setCount(count+1);
+  }
+
+
+  function decreaseHandler() {
+    setCount(count-1);
+  }
+
+  function resetHandler() {
+    setCount(0);
+  }
   return (
-    <h1>pritiranjan</h1>
+    <div className="container">
+      <div className="increamentdecreament">Increament & Decreament</div>
+
+      <div className="box">
+        <button onClick={decreaseHandler} className='bdecreaseHandler'>-</button>
+        <div>
+          {count}
+        </div>
+        <button onClick={increaseHandler}>
+          +
+        </button>
+      </div>
+      <button onClick={resetHandler} className="reset">
+        reset
+      </button>
+    </div>
   );
 }
 
